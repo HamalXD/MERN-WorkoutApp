9e3022d16d14622cbafa7c87fc6428756c543559
hamalxd <sammanhamal06@gmail.com>
2023-12-03 14:08:01 +0545

    Backend Completed

--- backend/contollers/workoutController.js
+++ backend/contollers/workoutController.js
@@ -28,6 +28,25 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
  console.log(title, load, reps)
+
+ let emptyFields = []
+
+ if (!title) {
+  emptyFields.push('title')
+ }
+
+ if (!load) {
+  emptyFields.push('load')
+ }
+
+ if (!reps) {
+  emptyFields.push('reps')
+ }
+
+ if (emptyFields.length > 0) {
+  return res.status(400).json({error: 'Please fill in all the details to add a workout', emptyFields})
+ }
+
   //ADD DOCS TO DB
   try {
     const workout = await Workout.create({ title, load, reps });
