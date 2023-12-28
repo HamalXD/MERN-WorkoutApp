552ff441d831dac5856451abc26daa50818da241
hamalxd <sammanhamal06@gmail.com>
2023-12-28 18:49:40 +0545

    Added encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,26 @@
-const User = require("../models/userModel")
+const User = require("../models/userModel");
 
 //LOGIN USER
-const loginUser = async(req,res) => {
-    res.json({msg : 'LOGIN USER'})
-}
+const loginUser = async (req, res) => {
+  res.json({ mssg: "LOGIN USER" });
+};
 
 //SIGNUP USER
-const signupUser = async(req,res) => {
-    res.json({msg : 'SIGNUP USER'})
-}
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.msg });
+  }
+
+  res.json({ mssg: "SIGNUP USER" });
+};
 
 module.exports = {
-    loginUser,
-    signupUser
-}
+  loginUser,
+  signupUser,
+};
